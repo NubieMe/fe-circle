4dc30d318b20051039128b78cb40c1590ddcc6e5
NubieMe <fahmi.mellow@gmail.com>
2024-04-23 21:57:53 +0700

    fix: change to the real base URL

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const API = axios.create({
-    baseURL: "be-circle-production.up.railway.app:5000/api/v1",
+    baseURL: "https://be-circle-production.up.railway.app/api/v1",
     headers: {
         Accept: "application/json",
         Authorization: Cookies.get("C.id") ? "Bearer " + Cookies.get("C.id") : "",
