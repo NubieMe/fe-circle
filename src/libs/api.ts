048af9453758a49fb61d807ac3af518fb3c4c4ca
NubieMe <fahmi.mellow@gmail.com>
2024-04-23 20:44:32 +0700

    update: changing base URL

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api/v1",
+    baseURL:
+        "https://railway.app/project/2b7f01e1-2039-4009-ab5e-a1ae2cc6069f/service/a0627719-16f8-4da7-b5a9-81982c48bb9c/:5000/api/v1",
     headers: {
         Accept: "application/json",
         Authorization: Cookies.get("C.id") ? "Bearer " + Cookies.get("C.id") : "",
