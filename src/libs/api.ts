ef7e1d70bd0c083ef2370efa769a17ae77ea5193
Fahmi Abdul Hadi <111063524+NubieMe@users.noreply.github.com>
2025-03-20 11:44:27 +0700

    Update api.ts
    update: change to temp baseURL

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const API = axios.create({
-    baseURL: "https://modern-honeybee-romantic.ngrok-free.app/",
+    baseURL: "https://modern-honeybee-romantic.ngrok-free.app/api/",
     headers: {
         Accept: "application/json",
         Authorization: Cookies.get("C.id") ? "Bearer " + Cookies.get("C.id") : "",
