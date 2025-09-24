1b142ba5a4960a892b747300e64598fe04f61bb6
fahmi <fahmi.mellow@gmail.com>
2025-09-24 15:41:03 +0700

    remove 1 /

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const API = axios.create({
-    baseURL: "http://3.106.220.128:5000/api/",
+    baseURL: "http://3.106.220.128:5000/api",
     headers: {
         Accept: "application/json",
         Authorization: Cookies.get("C.id") ? "Bearer " + Cookies.get("C.id") : "",
