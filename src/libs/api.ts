8b6d19e00ad08765cb4c4aca857d037602b0fb98
fahmi <fahmi.mellow@gmail.com>
2025-09-24 21:11:55 +0700

    add missing v1

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const API = axios.create({
-    baseURL: "https://stage-connections-flash-gorgeous.trycloudflare.com/api",
+    baseURL: "https://stage-connections-flash-gorgeous.trycloudflare.com/api/v1",
     headers: {
         Accept: "application/json",
         Authorization: Cookies.get("C.id") ? "Bearer " + Cookies.get("C.id") : "",
