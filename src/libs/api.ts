d66c6b84f26275d66a1076c90845a07d4cc2c646
fahmi <fahmi.mellow@gmail.com>
2025-09-24 22:30:54 +0700

    f, just use ngrok

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const API = axios.create({
-    baseURL: "https://stage-connections-flash-gorgeous.trycloudflare.com/api/v1",
+    baseURL: "https://modern-honeybee-romantic.ngrok-free.app/api/v1",
     headers: {
         Accept: "application/json",
         Authorization: Cookies.get("C.id") ? "Bearer " + Cookies.get("C.id") : "",
